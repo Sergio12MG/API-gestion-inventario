9dee9e52569b057c72e8d1e077bba52d61fb2e12
Sergio Mejia <sergio12mg25@gmail.com>
2025-05-23 13:10:55 -0500

    Cambios en los nombres de las rutas

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -4,18 +4,18 @@ const productoController = require('../controllers/producto.controller');
 const { authenticateJWT, authorizeRoles } = require('../middlewares/auth.middleware');
 
 // Obtener todos los productos (accesible para clientes y proveedores autenticados)
-router.get('/productos', authenticateJWT, authorizeRoles(['cliente', 'proveedor']), productoController.obtenerProductos);
+router.get('/obtener', authenticateJWT, authorizeRoles(['cliente', 'proveedor']), productoController.obtenerProductos);
 
 // Obtener un producto por ID (accesible para clientes y proveedores autenticados)
-router.get('/productos/:id', authenticateJWT, authorizeRoles(['cliente', 'proveedor']), productoController.obtenerProductoPorId);
+router.get('/obtener/:id', authenticateJWT, authorizeRoles(['cliente', 'proveedor']), productoController.obtenerProductoPorId);
 
 // Crear un producto (solo para proveedores autenticados)
-router.post('/productos', authenticateJWT, authorizeRoles(['proveedor']), productoController.crearProducto);
+router.post('/crear', authenticateJWT, authorizeRoles(['proveedor']), productoController.crearProducto);
 
 // Actualizar un producto (solo para el proveedor dueño del producto)
-router.put('/productos/:id', authenticateJWT, authorizeRoles(['proveedor']), productoController.actualizarProducto);
+router.put('/actualizar/:id', authenticateJWT, authorizeRoles(['proveedor']), productoController.actualizarProducto);
 
 // Eliminar un producto (solo para el proveedor dueño del producto)
-router.delete('/productos/:id', authenticateJWT, authorizeRoles(['proveedor']), productoController.eliminarProducto);
+router.delete('/eliminar/:id', authenticateJWT, authorizeRoles(['proveedor']), productoController.eliminarProducto);
 
 module.exports = router;
